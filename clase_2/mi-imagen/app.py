e00147e9e7ae3fe69656eb0cd457a90b83cf1ec2
juliamorandini <j.morandini@alumno.um.edu.ar>
2026-03-31 13:04:50 -0300

    practica de clase 2

--- clase_2/mi-imagen/app.py
+++ clase_2/mi-imagen/app.py
@@ -1,5 +1,5 @@
 import cowsay
 import sys
 
-mensaje = ' '.join(sys.argv[1:]) if len(sys.argv) > 1 else "Hola Dockerr!"
+mensaje = ' '.join(sys.argv[1:]) if len(sys.argv) > 1 else "Hola Docker!"
 cowsay.cow(mensaje)
